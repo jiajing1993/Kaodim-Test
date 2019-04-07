8e0907751214c15332373b4d404a2ce592b90a21
Jia Jing <jiajing19932@gmail.com>
2019-04-07 10:20:40 +0800

    chore: delete unused file and load data into app

---
+++ src/components/inputBuilder.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class inputBuilder extends Component {
+  render() {
+    return (
+      <div>
+        <p>{this.props.title}</p>
+      </div>
+    )
+  }
+}
