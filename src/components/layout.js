dce78f1412d5b5702f4da00c89b0f16358e62a08
Jia Jing <jiajing19932@gmail.com>
2019-04-08 10:47:21 +0800

    build: install sass loader

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,7 +8,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
-
+import '../style/layout.scss'
 const Layout = ({ children }) => (
   <StaticQuery
     query={graphql`
